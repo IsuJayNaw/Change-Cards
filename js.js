a49e59a87a63751400d429e74e7d7abf6ea57aa2
IsuJay <isujayawardana1220@gmail.com>
2021-01-25 18:42:13 +0530

    Change

--- js.js
+++ js.js
@@ -2,3 +2,9 @@ const proPrice = document.getElementById('proPrice');
 const proName = document.getElementById('proName');
 const buy = document.getElementById('buy');
 
+buy.addEventListener('click', (e) =>{
+    e.preventDefault();
+
+   
+
+});
